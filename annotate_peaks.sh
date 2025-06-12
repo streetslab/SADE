e212368a3657a8aa70caa019c1b9e2071f52f854
irisapo <syyangiris@gmail.com>
2025-06-12 15:44:29 -0700

    Visualize peak comaparisons

--- annotate_peaks.sh
+++ annotate_peaks.sh
@@ -18,11 +18,27 @@ done
 
 comparison_subdir="${output_dir}/peaks_comparison"
 newly_discovered_peaks=$comparison_subdir/newly_discovered_peaks.bed 
+lost_peaks=$comparison_subdir/lost_peaks.bed
 
 
 annotation_subdir="${output_dir}/annotation"
 mkdir -p $annotation_subdir
 annotated_newly_discovered_peaks=$annotation_subdir/annotated_newly_discovered_peaks.bed
+annotated_lost_peaks=$annotation_subdir/annotated_lost_peaks.bed
 
 
-annotatePeaks.pl $newly_discovered_peaks $genome_name > $annotated_newly_discovered_peaks
+# annotate peaks differences 
+annotatePeaks.pl $newly_discovered_peaks $genome_name -annStats  $annotation_subdir/"annStats_newly_discovered_peaks.txt" > $annotated_newly_discovered_peaks
+annotatePeaks.pl $lost_peaks $genome_name -annStats  $annotation_subdir/"annStats_lost_peaks.txt" > $annotated_lost_peaks
+
+
+
+# Annotate peaks 
+peak_calling_subdir="${output_dir}/peaks"
+peaks_bed_file="${peak_calling_subdir}/peaks.bed"
+
+entropy_peak_calling_subdir="${output_dir}/peaks_entropy_filtered"
+entropy_peaks_bed_file="${entropy_peak_calling_subdir}/peaks.bed"
+
+annotatePeaks.pl $peaks_bed_file $genome_name -annStats $annotation_subdir/"annStats_peaks.txt" > $annotation_subdir/annotated_peaks.bed
+annotatePeaks.pl $entropy_peaks_bed_file $genome_name -annStats $annotation_subdir/"annStats_entropy_peaks.txt" > $annotation_subdir/annotated_entropy_peaks.bed
---
+++ visulize_annotation.py
@@ -0,0 +1,98 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+#%%
+def simplify_annotation(annot_df, inplace=False):
+    """
+    Simplify the Annotation column.
+    """
+    if not inplace:
+        annot_df = annot_df.copy()
+        annot_df['simple_annotation'] = annot_df['Annotation'].apply(lambda x: str(x).split('(')[0])  
+        return annot_df
+    else:
+        # in place change of the  DataFrame
+        annot_df['simple_annotation'] = annot_df['Annotation'].apply(lambda x: str(x).split('(')[0])  
+        
+
+
+
+
+
+#%% 
+output_dir ='/mnt/hdd_bob/syy/adipose/atac/res/AB_ATAC_FL_TAM_SQ'
+annotation_dir = os.path.join(output_dir, 'annotation')
+
+figures_subdir = os.path.join(annotation_dir, 'figures')
+if not os.path.exists(figures_subdir):
+    os.makedirs(figures_subdir)
+# %%
+
+peaks_annot = pd.read_csv(os.path.join(annotation_dir, 'annotated_peaks.bed'), \
+    sep='\t')
+# %%
+entropy_peaks_annot = pd.read_csv(os.path.join(annotation_dir, 'annotated_entropy_peaks.bed'), \
+    sep='\t')
+# %%
+
+lost_peaks_annot = pd.read_csv(os.path.join(annotation_dir, 'annotated_lost_peaks.bed'), \
+    sep='\t')
+# %%
+newly_discovered_peaks_annot = pd.read_csv(os.path.join(annotation_dir, 'annotated_newly_discovered_peaks.bed'), \
+    sep='\t')
+#%% 
+
+simplify_annotation(peaks_annot, inplace=True)
+simplify_annotation(entropy_peaks_annot, inplace=True)
+# %%
+
+
+fig, ax = plt.subplots()
+ax.plot(peaks_annot['Peak Score'].sort_values(ascending=False), '-', color='red', alpha=0.5, label='Peaks')
+ax.plot(entropy_peaks_annot['Peak Score'].sort_values(ascending=False), '-', color='blue', alpha=0.5, label='Entropy Peaks')
+ax.plot(lost_peaks_annot['Peak Score'].sort_values(ascending=False), '-', color='gray', alpha=0.5, label='Lost Peaks')
+ax.plot(newly_discovered_peaks_annot['Peak Score'].sort_values(ascending=False), '-', color='green', alpha=0.5, label='Newly Discovered Peaks')
+ax.set_title('Peak Score Comparison')
+ax.set_xlabel('Peak rank')
+ax.set_ylabel('Peak Score')
+ax.legend( loc='upper right', bbox_to_anchor=(1.5, .6))
+# fig_file = os.path.join(figures_subdir, 'peak_score_comparison.png')
+# plt.savefig(fig_file)
+# %%
+
+    
+
+
+
+#%% 
+peaks_freq = peaks_annot['simple_annotation'].value_counts()
+entropy_peaks_freq = entropy_peaks_annot['simple_annotation'].value_counts()
+freq_df = pd.DataFrame({
+    'Peaks': peaks_freq,
+    'Entropy Peaks': entropy_peaks_freq
+}).fillna(0)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+freq_df.plot(kind='bar', figsize=(10, 6), width=0.8, alpha=0.7, ax=ax)
+# fig_file = os.path.join(figures_subdir, 'peak_annotation_frequency.png')
+# plt.savefig(fig_file)
+
+
+
+
+
+#fig, ax = plt.subplots(figsize=(10, 6))
+
+
+
+
+# peaks_annStat = pd.read_csv(os.path.join(annotation_dir, 'annStats_peaks.txt'), \
+#     index_col=0, sep='\t')
+
+# entropy_peaks_annStat = pd.read_csv(os.path.join(annotation_dir, 'annStats_entropy_peaks.txt'), \
+#     index_col=0, sep='\t')
+# %%
+
