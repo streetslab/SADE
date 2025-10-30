938fe7e5ec323496680c1439bb478d1eec8cf4e6
irisapo <syyangiris@gmail.com>
2025-10-30 14:40:33 -0700

    Graft finalized thresholding strategy onto Kevin's code

--- autothreshold.py
+++ autothreshold.py
@@ -7,7 +7,7 @@ from scipy.interpolate import BSpline, CubicSpline, make_interp_spline, make_spl
 from typing import Iterable
 
 
-def fit_spline_and_find_cutoff(x: np.ndarray, y: np.ndarray, k: int=2, cutoff: float=6e-8, spline_s: int=67, spline_k: int=3) -> tuple[float, int, BSpline]:
+def fit_spline_and_find_cutoff(x: np.ndarray, y: np.ndarray, k: int=2, spline_s: int=67, spline_k: int=3, limit: int=20000) -> tuple[float, int, BSpline]:
     """
     Takes the ranks (x) and the corresponding log 10 entropy values (y) and returns estimated entropy cutoff, the rank cutoff, and the scipy BSpline object used to fit the curve.
     The function fits a smoothing spline to the curve, then computes the first and second derivatives on the spline. then iterating on increasing x values, finds regions where the left bound of the region is where second derivative transitions from greater than cutoff to below cutoff, and then the right bound is where second derivative goes from below cutoff to above. then takes the kth such region by ranks, and computes the greatest dropoff in the region (min second derivative). returns this point as cutoff 
@@ -22,35 +22,50 @@ def fit_spline_and_find_cutoff(x: np.ndarray, y: np.ndarray, k: int=2, cutoff: f
     returns: tuple[float, int, Bspline]
     first value is entropy cutoff, second value is rank cutoff, third is the spline fit to the curve and used to find cutoffs
     """
+    # only consider up to limit barcodes for spline fitting (as even the highest throughput datasets have < 20k cells)
+    x = x[:limit]
+    y = y[:limit]
     cs = make_splrep(x, y, k=spline_k, s=spline_s)
+    
+    first_deriv = cs.derivative(1)(x)
+    second_deriv = cs.derivative(2)(x)
+    second_deriv = second_deriv / np.abs(second_deriv).max()  # normalize second derivative to better find inflection regions
+    
     left = 0
     right = 0
     flag = False
     count = 0 
-    final_left = 0
-    for i in x: 
-        first_deriv = splev(i, cs, der=1)
-        second_deriv = splev(i, cs, der=2)
 
-        if (not flag) and second_deriv > cutoff:
+    inflection_regions = {}
+    
+    for i in x: 
+        
+        d_2 = second_deriv[i]
+        if (not flag) and d_2 < 0:
             flag = True
             left = i
-        elif flag and second_deriv < cutoff:
+        elif flag and d_2 > 0:
             flag = False
             right = i
             count += 1
-
+            
+        inflection_regions[count] = (left, right) # store inflection region boundaries
+        
         if count == k: 
             break
 
-    if left > right:
-        right = x[-1]
-    d = {}
-    for i in range(len(x)):
-        if i >= left and i <= right:
-            d[x[i]] = y[i]
-    rank = min(d, key=d.get)
-    return rank, d[rank], cs
+    # find the global minimum first derivative among the k inflection regions found
+    regions_rank = []
+    for region_index, (region) in inflection_regions.items():
+        left, right = region
+        if left > right:
+            right = x[-1]
+        rank = np.argmin(first_deriv[left:right+1]) + left
+        regions_rank.append(rank)
+
+    rank = regions_rank[np.argmin(first_deriv[regions_rank])]
+    
+    return rank, y[rank], cs
 
 def get_x_y_from_pickle_helper(pickle_path: str):
     with open(pickle_path, 'rb') as f:
@@ -71,14 +86,16 @@ def make_plots(pickle_path, save_path):
     for i in range(1, 3):
         deriv[i] = cs.derivative(i)(x)
 
+    deriv[2] = deriv[2] / np.abs(deriv[2]).max()  # normalize second derivative for better visualization    
+    
     fig, ax = plt.subplots(3, 1, figsize=(10,10), dpi=300)
     ax[0].plot(x, y, 'o', label='data')
     ax[0].plot(x, spl_y, label='spline')
     ax[1].plot(x, deriv[1], label='first derivative')
     ax[2].plot(x, deriv[2], label='second derivative')
 
-    ax[1].set_ylim(-0.001, 0.001)
-    ax[2].set_ylim(-1e-7, 1e-7)
+    d1_limit = np.abs(deriv[1]).max()
+    ax[1].set_ylim(-d1_limit*1.1, d1_limit*1.1)
 
     for i in range(3):
         ax[i].axvline(rank_cutoff, color='pink', linestyle='--', label='rank_cutoff')
