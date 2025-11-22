08bf67f042f8464d9dde1207f11c823259f900d9
irisapo <syyangiris@gmail.com>
2025-11-22 14:08:22 -0800

    Read file compatible

--- refine_figures.py
+++ refine_figures.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
 
     entropy_file = os.path.join(output_dir, f'{chromosome}_barcode_entropy_df.tsv')
-    entropy_df = pd.read_csv(entropy_file, sep='\t', header=0, index_col=0)
+    entropy_df = pd.read_csv(entropy_file, sep=',', header=0, index_col=0)
     entropy_df['pass'] = entropy_df['Entropy'] > EntropyThreshold
 
     entropy = entropy_df['Entropy'].dropna().values # remove None values   
