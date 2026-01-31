ab36881177646eada1107790aee8ba66ceb0445e
irisapo <syyangiris@gmail.com>
2026-01-30 23:42:02 -0800

    Update figure color

--- visualize_fragments_overlap_peaks.py
+++ visualize_fragments_overlap_peaks.py
@@ -51,23 +51,23 @@ if __name__ == "__main__":
 
     #%% 
     fig, ax = plt.subplots(figsize=(10, 6))
-    color_map = {'Overlap with Peaks':'blue', 'Overlap with Entropy Peaks':'orange'}
+    color_map = {'Overlap with Peaks':'orange', 'Overlap with Entropy Peaks':'blue'}
     ax.scatter(
         np.log10(frag_overlap_peaks_df['total_fragments'].values),
         frag_overlap_peaks_df['frag_overlap_peaks%'].values,
-        s=2, alpha=0.5, label='Overlap with Peaks', color='blue'
+        s=2, alpha=0.5, label='Overlap with Peaks', color='orange'
     )
     ax.scatter(
         np.log10(frag_overlap_entropypeaks_df['total_fragments'].values),
         frag_overlap_entropypeaks_df['frag_overlap_entropy_peaks%'].values,
-        s=2, alpha=0.5, label='Overlap with Entropy Peaks', color='orange'
+        s=2, alpha=0.5, label='Overlap with Entropy Peaks', color='blue'
     )
     ax.set_xlabel('Log10 of total fragments', fontsize=14)
     ax.set_ylabel(f'Fragments% overlapping with peaks', fontsize=14)
     fig.legend(handles=[Line2D([0], [0], marker='o', color='w', label='Overlap with Peaks',
-                          markerfacecolor='blue', markersize=10),
+                          markerfacecolor='orange', markersize=10),
                  Line2D([0], [0], marker='o', color='w', label='Overlap with Entropy Peaks',
-                           markerfacecolor='orange', markersize=10)],
+                           markerfacecolor='blue', markersize=10)],
            bbox_to_anchor=(1.02, 1), loc='upper left')
     fig_file = os.path.join(figure_subdir, 'fragments_overlap_peaks_scatter.png')
     fig.savefig(fig_file, dpi=800, bbox_inches='tight')
