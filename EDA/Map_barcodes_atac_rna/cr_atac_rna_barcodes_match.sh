360183dbffd0b9cb850e46af29a8ab0c5dba5657
irisapo <syyangiris@gmail.com>
2026-09-02 23:57:59 -0700

    Set main branch ignore analysis result

--- EDA/Map_barcodes_atac_rna/cr_atac_rna_barcodes_match.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gzip -cd  cellranger-arc-2.0.2/lib/python/cellranger/barcodes/737K-arc-v1.txt.gz  > /mnt/files/syy/adipose/multiom/Arionas_data_May2023/Analysis/SCVI_integration/rna_barcodes.tsv
-gzip -cd  cellranger-arc-2.0.2/lib/python/atac/barcodes/737K-arc-v1.txt.gz  > /mnt/files/syy/adipose/multiom/Arionas_data_May2023/Analysis/SCVI_integration/atac_barcodes.tsv
-
-paste atac_barcodes.tsv  rna_barcodes.tsv | awk -v OFS='\t' '{print , }' >  atac_rna_barcodes_map.tsv
--- EDA/Preprocessing_fastqs/mo_v2_run_atac_fastq.sh
+++
@@ -1,9 +0,0 @@
-# ATAC
-
-CR_atac='/home/syyang/CR/cellranger-atac-2.1.0/bin/cellranger-atac'
-$CR_atac count --id=VIB_10xmultiome_2 \
-                        --chemistry=ARC-v1 \
-                        --reference=/home/syyang/CR/refdata-cellranger-arc-GRCh38-2020-A-2.0.0 \
-                        --fastqs=/mnt/hdd_bob/syy/adipose/atac/protocol_benchmark/fastq/MO_V2/atac_SRR24422953 \
-                        --localcores=30 \
-                        --localmem=100 
